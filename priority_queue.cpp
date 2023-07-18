b21d80cf14998ec92225d674f7d89727135e0aba
anisul islam <anisulislam980340@gmail.com>
2023-07-18 22:46:34 +0600

    max priority heap completed

---
+++ priority_queue.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    priority_queue<int>pq;
+    while(true){
+        int c;
+        cin>>c;
+        if(c==0){
+            int x;
+            cin>>x;
+            pq.push(x);
+        }
+        else if(c==1){
+            pq.pop();
+        }
+        else if(c==2){
+            pq.top();
+        }
+        else {
+            break;
+        }
+    }
+    return 0;
+}
